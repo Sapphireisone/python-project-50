f11cd8c6b59099e21640cac752901dcd1f420396
Sapphire <allmediasale@icloud.com>
2024-01-05 18:50:53 +0700

    changed 17-s string in stylish

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -14,7 +14,7 @@ def stylish(in_data, depth=0, result='{\n', end='}'):
         elif type == "unchanged":
             result += f"{shift}  {key}: {value}\n"
         else:
-            result += shift + key + ": {\n"
+            result += f"{shift}  {key}" + ": {\n"
             result += f"{stylish(value, depth + 2, result='', end=' ')}\n"
             result += shift + "}\n"
 
