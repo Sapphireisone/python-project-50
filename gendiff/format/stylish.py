8bf2afa71267d6dde76f0e2cdb031173a3457d55
Sapphire <allmediasale@icloud.com>
2024-01-07 00:51:32 +0700

    changed shifts in function string in stylish.py

--- gendiff/format/stylish.py
+++ gendiff/format/stylish.py
@@ -1,22 +1,32 @@
-def stylish(in_data, depth=0, result='{\n', end='}'):
-    shift = "  " + ("  " * depth)
-    for dictionary in in_data:
-        key = dictionary["key"]
-        value = dictionary["value"]
-        type = dictionary["type"]
+def stringer(value, depth=0):
+    shift = ("    " * depth) + "    "
+    result = ""
+    if isinstance(value, dict):
+        result += "{\n"
+        for key, val in value.items():
+            result += f"{shift}{shift}{key}: {stringer(val, depth+1)}\n"
+        result += shift + "}"
+    else:
+        result = value
+    return result
+
+
+def stylish(data, depth=0, result='{\n', end='}'):
+    shift = ("    " * depth) + "  "
+    for dic in data:
+        key = dic["key"]
+        type = dic["type"]
         if type == "added":
-            result += f"{shift}+ {key}: {value}\n"
+            result += f"{shift}+ {key}: {stringer(dic['value'])}\n"
         elif type == "deleted":
-            result += f"{shift}- {key}: {value}\n"
+            result += f"{shift}- {key}: {stringer(dic['value'])}\n"
         elif type == "changed":
-            result += f"{shift}- {key}: {value[0]}\n"
-            result += f"{shift}+ {key}: {value[1]}\n"
+            result += f"{shift}- {key}: {stringer(dic['value'][0])}\n"
+            result += f"{shift}+ {key}: {stringer(dic['value'][1])}\n"
         elif type == "unchanged":
-            result += f"{shift}  {key}: {value}\n"
+            result += f"{shift}  {key}: {stringer(dic['value'])}\n"
         else:
             result += f"{shift}  {key}" + ": {\n"
-            result += f"{stylish(value, depth + 2, result='', end=' ')}\n"
-            result += shift + "}\n"
-
-    result += end
-    return result 
+            result += f"{stylish(stringer(dic['value']), depth + 1, result='', end=' ')}\n"
+            result += shift + "  }\n"
+    return result + end
